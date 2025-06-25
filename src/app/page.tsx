eb42cdbb31e9aa025bc8ba5d2191c88dabd5d13a
danpgodwin <danpgodwin@gmail.com>
2025-06-25 18:54:51 +0100

    v21

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-/// Using a direct relative path avoids reliance on path-alias configuration
 import Calculator from "../components/Calculator"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Calculator />
-    </main>
-  )
+  return <Calculator />
 }
